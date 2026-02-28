b410e63ff293c722db4aa7c2c74acc354c54de57
Jetross <jetrossneri07@gmail.com>
2026-02-28 09:03:27 +0800

    refactor: change the title of the system

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,11 +1,17 @@
-import { TypographyH1 } from "../components/typography";
+import { TypographyH1, TypographyP } from "../components/typography";
 import Navbar from "../components/navbar";
 
 export default function LandingPage() {
   return (
     <main>
       <Navbar brandName="Basics" />
-      <TypographyH1>Welcome to the Landing Page</TypographyH1>
+      <div className="mt-20 flex flex-col items-center justify-center">
+        <TypographyH1>Welcome to the Basics of Scripting!</TypographyH1>
+        <TypographyP>
+          This website is just for practice purposes only to master scripting
+          languages
+        </TypographyP>
+      </div>
     </main>
   );
 }
