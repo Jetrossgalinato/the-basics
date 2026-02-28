9574bb8e78522a9925c0dfc8129de24a6eb03882
Jetross <jetrossneri07@gmail.com>
2026-02-28 08:48:25 +0800

    feat: add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/navbar";
+
+export default function AboutPage() {
+  return (
+    <div className="min-h-screen">
+      <Navbar brandName="Basics" />
+    </div>
+  );
+}
