a2c381e5ed98af1788da20e32ed671fd3f06d216
Jetross <jetrossneri07@gmail.com>
2026-02-28 11:45:25 +0800

    feat: add projectData and imported it (projects)

---
+++ app/projects/data/projectData.ts
@@ -0,0 +1,11 @@
+export const projectData = [
+  {
+    id: 1,
+    title: "Interactive Counter",
+    description: "simple widget with increment, decrement, and reset buttons",
+    imageSrc: "https://avatar.vercel.sh/shadcn1",
+    imageAlt: "Interactive Counter",
+    badgeText: "Featured",
+    link: "/projects/counter",
+  },
+];
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -11,6 +11,8 @@ import {
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
+import { projectData } from "./data/projectData";
+import Link from "next/link";
 
 export default function ProjectsPage() {
   return (
@@ -23,30 +25,34 @@ export default function ProjectsPage() {
           can review your progress as a developer.
         </TypographyP>
       </div>
-      <div className="mt-20 flex items-center justify-center">
-        <Card className="relative mx-auto w-full max-w-sm pt-0 overflow-hidden">
-          <div className="absolute inset-0 z-30 aspect-video bg-black/35" />
-          <Image
-            src="https://avatar.vercel.sh/shadcn1"
-            alt="Event cover"
-            width={800}
-            height={450}
-            className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
-          />
-          <CardHeader>
-            <CardAction>
-              <Badge variant="secondary">Featured</Badge>
-            </CardAction>
-            <CardTitle>Design systems meetup</CardTitle>
-            <CardDescription>
-              A practical talk on component APIs, accessibility, and shipping
-              faster.
-            </CardDescription>
-          </CardHeader>
-          <CardFooter>
-            <Button className="w-full">View Project</Button>
-          </CardFooter>
-        </Card>
+      <div className="mt-20 max-w-6xl mx-auto px-4 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {projectData.map((project) => (
+          <Card
+            key={project.id}
+            className="relative mx-auto w-full max-w-sm pt-0 overflow-hidden"
+          >
+            <div className="absolute inset-0 z-30 aspect-video bg-black/35" />
+            <Image
+              src={project.imageSrc}
+              alt={project.imageAlt}
+              width={800}
+              height={450}
+              className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+            />
+            <CardHeader>
+              <CardAction>
+                <Badge variant="secondary">{project.badgeText}</Badge>
+              </CardAction>
+              <CardTitle>{project.title}</CardTitle>
+              <CardDescription>{project.description}</CardDescription>
+            </CardHeader>
+            <CardFooter>
+              <Link href={project.link}>
+                <Button className="w-full">View Project</Button>
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
       </div>
     </div>
   );
