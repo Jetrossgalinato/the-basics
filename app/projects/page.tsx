89e5ac9250c7a164416852cdd7c23ebc5bf9a06b
Jetross <jetrossneri07@gmail.com>
2026-02-28 08:46:46 +0800

    feat: add projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/navbar";
+
+export default function ProjectsPage() {
+  return (
+    <div className="min-h-screen">
+      <Navbar brandName="Basics" />
+    </div>
+  );
+}
