22d67eeb29e1a8650b39357d181a506e5b438d8f
Jetross <jetrossneri07@gmail.com>
2026-03-01 18:26:50 +0800

    feat: add page title (counter)

--- app/projects/counter/page.tsx
+++ app/projects/counter/page.tsx
@@ -1,9 +1,16 @@
 import Navbar from "@/app/components/navbar";
+import { TypographyH1, TypographyP } from "@/app/components/typography";
 
 export default function CounterPage() {
   return (
     <div className="min-h-screen">
       <Navbar brandName="Basics" />
+      <div className="mt-30 flex flex-col items-center justify-center">
+        <TypographyH1>Interactive Counter</TypographyH1>
+        <TypographyP>
+          simple widget with increment, decrement, and reset buttons
+        </TypographyP>
+      </div>
     </div>
   );
 }
